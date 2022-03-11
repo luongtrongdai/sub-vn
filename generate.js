f48d7e5737b02c0d3904488fcb7246889fb22e83
Ken <ken@gmail.com>
2022-03-11 20:31:49 +0700

    add areas

--- generate.js
+++ generate.js
@@ -5,12 +5,30 @@ const EXCEL_PATH = path.resolve(__dirname, "./excel");
 const JSON_PATH = path.resolve(__dirname, "./json_data");
 const DEFAULT_SHEET = "Sheet1";
 const files = fs.readdirSync(EXCEL_PATH);
-let provinces = [],
+let areas = [],
+  provinces = [],
   districts = [],
   wards = [];
 let tree = {};
+let treeWithArea = {};
 files.forEach(file_name => {
-  if (file_name.includes("tỉnh")) {
+  if (file_name.includes("vùng")) {
+    const areas_workbook = XLSX.readFile(
+      path.resolve(EXCEL_PATH, file_name)
+    ).Sheets[DEFAULT_SHEET];
+    areas = XLSX.utils
+      .sheet_to_json(areas_workbook, { raw: true })
+      .map(x => ({
+        code: x["Mã"],
+        name: x["Tên"],
+        unit: x["Cấp"]
+      }))
+      .filter(x => x.code);
+    fs.writeFileSync(
+      path.resolve(JSON_PATH, "areas.json"),
+      JSON.stringify(areas, null, 2)
+    );
+  } else if (file_name.includes("tỉnh")) {
     const provinces_workbook = XLSX.readFile(
       path.resolve(EXCEL_PATH, file_name)
     ).Sheets[DEFAULT_SHEET];
@@ -19,7 +37,10 @@ files.forEach(file_name => {
       .map(x => ({
         code: x["Mã"],
         name: x["Tên"],
-        unit: x["Cấp"]
+        unit: x["Cấp"],
+        area_code: x["Mã vùng"],
+        area_name: x["Vùng"],
+        full_name: `${x["Tên"]}, ${x["Vùng"]}`
       }))
       .filter(x => x.code);
     fs.writeFileSync(
@@ -38,7 +59,9 @@ files.forEach(file_name => {
         unit: x["Cấp"],
         province_code: x["Mã TP"],
         province_name: x["Tỉnh / Thành Phố"],
-        full_name: `${x["Tên"]}, ${x["Tỉnh / Thành Phố"]}`
+        area_code: x["Mã vùng"],
+        area_name: x["Vùng"],
+        full_name: `${x["Tên"]}, ${x["Tỉnh / Thành Phố"]}, ${x["Vùng"]}`
       }))
       .filter(x => x.code);
     fs.writeFileSync(
@@ -58,7 +81,9 @@ files.forEach(file_name => {
         district_name: x["Quận Huyện"],
         province_code: x["Mã TP"],
         province_name: x["Tỉnh / Thành Phố"],
-        full_name: `${x["Tên"]}, ${x["Quận Huyện"]}, ${x["Tỉnh / Thành Phố"]}`
+        area_code: x["Mã vùng"],
+        area_name: x["Vùng"],
+        full_name: `${x["Tên"]}, ${x["Quận Huyện"]}, ${x["Tỉnh / Thành Phố"]}, ${x["Vùng"]}`
       }))
       .filter(x => x.code);
     fs.writeFileSync(
@@ -67,6 +92,38 @@ files.forEach(file_name => {
     );
   }
 });
+
+areas.forEach(area => {
+  treeWithArea[area.code] = area;
+});
+
+provinces.forEach(province => {
+  if (!treeWithArea[province.area_code].provinces) {
+    treeWithArea[province.area_code].provinces = {};
+  }
+  treeWithArea[province.area_code].provinces[province.code] = province;
+});
+
+districts.forEach(district => {
+  if (!treeWithArea[district.area_code].provinces[district.province_code].districts) {
+    treeWithArea[district.area_code].provinces[district.province_code].districts = {};
+  }
+  treeWithArea[district.area_code].provinces[district.province_code].districts[district.code] = district;
+});
+
+wards.forEach(ward => {
+  if (!treeWithArea[ward.area_code].provinces[ward.province_code].districts[ward.district_code].wards) {
+    treeWithArea[ward.area_code].provinces[ward.province_code].districts[ward.district_code].wards = {};
+  }
+  treeWithArea[ward.area_code].provinces[ward.province_code].districts[ward.district_code].wards[
+    ward.code
+  ] = ward;
+});
+fs.writeFileSync(
+  path.resolve(JSON_PATH, "treeWithArea.json"),
+  JSON.stringify(tree, null, 2)
+);
+
 
 provinces.forEach(province => {
   tree[province.code] = province;
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const areas = require("./json_data/areas");
 const provinces = require("./json_data/provinces");
 const districts = require("./json_data/districts");
 const wards = require("./json_data/wards");
 const tree = require("./json_data/tree");
+const treeWithArea = require("./json_data/treeWithArea");
 module.exports = {
+  getAreas: () => areas,
   getProvinces: () => provinces,
   getDistricts: () => districts,
   getWards: () => wards,
+  getAreasWithDetail: () => treeWithArea,
   getProvincesWithDetail: () => tree,
+  getProvindByAreaCode: (areaCode) => provinces.filter(x => x.areaCode == areaCode),
   getDistrictsByProvinceCode: (provinceCode) => districts.filter(x => x.province_code == provinceCode),
   getWardsByDistrictCode: (districtCode) => wards.filter(x => x.district_code == districtCode),
   getWardsByProvinceCode: (provinceCode) => wards.filter(x => x.province_code == provinceCode),
